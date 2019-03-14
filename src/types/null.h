b95e446914cd83a2f94f17bbbc369b90b43c232b
imhele <work@imhele.com>
2019-03-14 18:15:22 +0800

    try start with prototype

---
+++ src/types/null.h
@@ -0,0 +1,9 @@
+#ifndef __HLIB_NULL
+#define __HLIB_NULL
+
+/**
+ ** null
+ */
+#define null ((void *)0)
+
+#endif
--- src/types/object.h
+++ src/types/object.h
@@ -1,9 +1,30 @@
 #ifndef __HLIB_OBJECT
 #define __HLIB_OBJECT
-#include <stdio.h>
 
+/**
+ * Basic object type
+ */
 typedef struct Object
 {
+  Object *prototype;
 } Object;
+
+typedef struct ObjectInstance
+{
+  Object *__proto__;
+  ObjectInstance (*valueOf)(void);
+} ObjectInstance;
+
+int main()
+{
+  ObjectInstance *p;
+  p->valueOf = valueOf;
+}
+
+ObjectInstance *valueOf(void)
+{
+  ObjectInstance *p;
+  return p;
+}
 
 #endif
