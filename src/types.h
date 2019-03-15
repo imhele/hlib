cf2d0d0d4186f5aac6a103b5741ff7b96c4b7519
imhele <work@imhele.com>
2019-03-15 11:59:21 +0800

    closure

--- src/types.h
+++ src/types.h
@@ -20,36 +20,61 @@
  * A symbol value is created as an anonymous, unique value. A symbol may be used as an object property.
  */
 #define symbol ((void *)2)
-
 /**
- * Basic object type
+ * Array
  */
+#define array ((void *)3)
+
+struct ClosureVar
+{
+  struct Object *var;
+  struct Object *next;
+};
+
+struct Closure
+{
+  struct ClosureVar *vars;
+  struct Closure *prev;
+  struct Closure **next;
+};
+
+struct Closure *Closure;
+
 struct Object
 {
   char *name;
   void *type;
-  struct Object **prototype;
+  void *value;
   struct Object *__proto__;
+  struct Object **prototype;
 };
 
 struct Object *Object;
-
-struct Symbol
-{
-  char *name;
-  void *type;
-  struct Object **prototype;
-  struct Object *__proto__;
-};
-
-struct Symbol *Symbol;
+struct Object *Symbol;
+struct Object *Array;
 
 void createSymbol(char *name);
 
 /**
  * Variables defined in a closure can be used anywhere in the closure after the definition of it.
+ * @param mode == 0: Create a new closure
+ * @param mode != 0: Destroy a closure
  */
-void useClosure(char exit);
+void useClosure(char mode)
+{
+  if (mode)
+  {
+    struct Closure *nextOne = (struct Closure *)malloc(sizeof(struct Closure));
+    nextOne->vars = (struct ClosureVar *)malloc(sizeof(struct ClosureVar));
+    if (Closure->next == null)
+    {
+      Closure->next = (struct Closure **)malloc(sizeof(struct Closure *));
+    }
+  }
+  else
+  {
+  }
+};
 
 void useSymbol(void);
 
@@ -59,21 +84,51 @@ void useSymbol(void);
 #define useObject(varName)
 
 /**
+ * Get the value of object
+ */
+void *useValue(struct Object *obj) {
+  return null;
+}
+
+/**
  * Initialize the contents of the entire library.
  */
 void useLib(void)
 {
+  /**
+   * Init Closure
+   */
+  Closure = (struct Closure *)malloc(sizeof(struct Closure));
+  Closure->prev = Closure;
+
+  /**
+   * Init Object
+   */
   Object = (struct Object *)malloc(sizeof(struct Object));
   Object->name = "object";
   Object->type = object;
   // Object->prototype = Object;
-  Object->__proto__ = null;
 
-  Symbol = (struct Symbol *)malloc(sizeof(struct Symbol));
+  /**
+   * Init Symbol
+   */
+  Symbol = (struct Object *)malloc(sizeof(struct Object));
   Symbol->name = "symbol";
   Symbol->type = symbol;
   // Symbol->prototype = Object;
-  Symbol->__proto__ = null;
+
+  /**
+   * Init Array
+   */
+  Array = (struct Object *)malloc(sizeof(struct Object));
+  Array->name = "array";
+  Array->type = array;
+  // Array->prototype = Object;
+
+  /**
+   * TODO remove test print
+   */
+  printf("%s", Symbol->prototype == NULL ? "true" : "false");
 }
 
 #endif /* __HLIB_SYMBOL */
